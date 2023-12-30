0e49e97a98138a8460ee93444f1207e689d67c2d
Pratik Jadhav <prateek7120@gmail.com>
2023-12-30 15:16:51 +0530

    1-Attempt

---
+++ src/main/java/com/driver/MovieService.java
@@ -0,0 +1,53 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    /* <-----------POST METHOD -------------*/
+    public void addMovie(Movie m)
+    {
+        movieRepository.addMovie(m);
+    }
+    public void addDirector(Director d)
+    {
+        movieRepository.addDirector(d);
+    }
+    /* <-----------PUT METHOD -------------*/
+    public void addMovieDirectorPair(String m,String d)
+    {
+       movieRepository.addMovieDirectorPair(m,d);
+    }
+
+    /* <-----------GET METHOD -------------*/
+    public Movie getMovieByname(String m)
+    {
+        return movieRepository.getMovieByname(m);
+    }
+    public Director getDirectorByName(String d)
+    {
+        return movieRepository.getDirectorByName(d);
+    }
+    public List<String> getMoviesByDiretorName(String d)
+    {
+        return movieRepository.getMoviesByDiretorName(d);
+    }
+    public List<String> findAllMovies()
+    {
+        return movieRepository.findAllMovies();
+    }
+    public void deleteDirectorByName(String d)
+    {
+        movieRepository.deleteDirectorByName(d);
+    }
+    public void deleteAllDirectors()
+    {
+        movieRepository.deleteAllDirectors();
+    }
+
+}
